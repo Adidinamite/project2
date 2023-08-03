f3e5a657bec92561f4feb6f8087b8d3ff1ea4141
Adidinamite <adrian.aparaschivei04@gmail.com>
2023-08-04 00:00:20 +0300

    Final Commit (?)

--- magic.js
+++ magic.js
@@ -1,13 +1,36 @@
 const addInputButton = document.getElementById("addInput")
-const listOfInputs=[];
+const container = document.getElementById("list");
+const taskList=[];
+let cnt = 0;
 const getInputValue =()=>{
+    taskList.push(cnt++);
     let inputVal = document.getElementById("textInput").value;
-    listOfInputs.push(inputVal);
-    for(let i = 0;i< listOfInputs.length;i++)
-    {
-        document.getElementById("output").innerHTML =listOfInputs[i];
-        //document.write("\n");
-    }
 
+    const content = document.createTextNode(inputVal);
+    const paragraph = document.createElement("p");
+    const task = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    const buttonText = document.createTextNode("X");
+    paragraph.classList.add("bubble");
+    deleteButton.setAttribute("id",cnt);
+    //deleteButton.setAttribute("id",(+cnt)+"-"+(+cnt) );
+    task.appendChild(content);
+    paragraph.appendChild(task);
+    container.appendChild(paragraph);
+    paragraph.appendChild(deleteButton);
+    deleteButton.appendChild(buttonText);
+    window.onclick = e => {
+        e.preventDefault();
+        const clickedButton = e.target;  // to get the element
+        console.log(clickedButton.innerText);
+        if(clickedButton.innerText === "X" ) {
+            clickedButton.parentNode.remove()
+        }
+    }
+    /*const deleteTask = () =>{
+        const element = document.getElementById("1");
+        element.parentNode.remove();
+    }
+    deleteButton.addEventListener("click",deleteTask);*/
 }
 addInputButton.addEventListener("click", getInputValue);
